e337f694366981eb3e973aac5933a8ee69495ac3
Shea Silverman <shea.silverman@ucf.edu>
2013-04-27 23:31:41 -0400

    basssssssshhhhh

--- update.sh
+++ update.sh
@@ -13,7 +13,7 @@ git clone https://github.com/ssilverm/PiMAME.git
 cd PiMAME
 git pull
 PREVGITFILE_mame=$(git show HEAD~1:.advance/advmame.rc |  md5sum | awk '{ print $1 }')
-CURRENTFILE_mame=$(/home/pi/.advance/advmame.rc |  md5sum | awk '{ print $1 }')
+CURRENTFILE_mame=$(cat /home/pi/.advance/advmame.rc |  md5sum | awk '{ print $1 }')
 if [ "$PREVGITFILE_mame" = "$CURRENTFILE_mame" ]; then
 	#DO COPY STUFF HERE
 	echo "Default AdvMAME configuration, updating."
@@ -24,7 +24,7 @@ else
 fi
 
 PREVGITFILE_menu=$(git show HEAD~1:.advance/advmenu.rc |  md5sum | awk '{ print $1 }')
-CURRENTFILE_menu=$(/home/pi/.advance/advmenu.rc |  md5sum | awk '{ print $1 }')
+CURRENTFILE_menu=$(cat /home/pi/.advance/advmenu.rc |  md5sum | awk '{ print $1 }')
 if [ "$PREVGITFILE_menu" = "$CURRENTFILE_menu" ]; then
 	#DO COPY STUFF HERE
 	echo "Default AdvMENU configuration, updating."
@@ -35,7 +35,7 @@ else
 fi
 
 PREVGITFILE_neo=$(git show HEAD~1:.gngeo/gngeorc |  md5sum | awk '{ print $1 }')
-CURRENTFILE_neo=$(/home/pi/.gngeo/gngeorc |  md5sum | awk '{ print $1 }')
+CURRENTFILE_neo=$(cat /home/pi/.gngeo/gngeorc |  md5sum | awk '{ print $1 }')
 if [ "$PREVGITFILE_neo" = "$CURRENTFILE_neo" ]; then
 	#DO COPY STUFF HERE
 	echo "Default GNGeo configuration, updating."
