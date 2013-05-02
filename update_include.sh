8d5f84c8f77154b0561b75549992d19a8980b8e7
Shea Silverman <shea.silverman@ucf.edu>
2013-05-02 18:03:30 -0400

    moving a file

--- update_include.sh
+++ update_include.sh
@@ -1,5 +1,8 @@
 echo "Checking update_include script"
-echo "Nothing here"
+echo "Updating..."
+wget http://socialcase.com/raspberrypi/gridlee.zip
+mv gridlee.zip /home/pi/roms/
+
 
 #increment
-echo "PiMAME is now version 0.6 Beta 6"
+echo "PiMAME is now version 0.6 Beta 6.1"
