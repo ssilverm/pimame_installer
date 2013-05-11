b3eba4d974af27a83e43694109dbfd9983b750e2
ssilverm <shea.silverman@ucf.edu>
2013-05-11 22:56:40 +0000

    editing update

--- update_include.sh
+++ update_include.sh
@@ -27,6 +27,7 @@ cd /home/pi/emulators/mame4all-pi/
 unzip mame4all_pi.zip
 ln -s /home/pi/emulators/mame4all-pi/roms/ /home/pi/roms/mame4all
 cd /home/pi/pimame_installer
+rm -rf mame4all-pi/
 
 
 cp PiMAME/pimame_files/menu.py /home/pi/pimame_files/menu.py
