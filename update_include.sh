dc8b73a86ed9c615c2b728100d7ff84eb5e29515
ssilverm <shea.silverman@ucf.edu>
2013-05-11 23:57:09 +0000

    adding fixs

--- update_include.sh
+++ update_include.sh
@@ -13,7 +13,7 @@ wget https://pisnes.googlecode.com/files/pisnes.zip
 mkdir /home/pi/emulators/pisnes
 mv pisnes.zip /home/pi/emulators/pisnes
 cd /home/pi/emulators/pisnes/
-unzip pisnes.zip
+unzip -o pisnes.zip
 ln -s /home/pi/emulators/pisnes/roms/ /home/pi/roms/snes
 cd /home/pi/pimame_installer
 
@@ -24,12 +24,14 @@ git clone https://code.google.com/p/mame4all-pi/
 mkdir /home/pi/emulators/mame4all-pi/
 cp mame4all-pi/mame4all_pi.zip /home/pi/emulators/mame4all-pi/
 cd /home/pi/emulators/mame4all-pi/
-unzip mame4all_pi.zip
+unzip -o mame4all_pi.zip
 ln -s /home/pi/emulators/mame4all-pi/roms/ /home/pi/roms/mame4all
 cd /home/pi/pimame_installer
 rm -rf mame4all-pi/
 
-
+cd PiMAME
+git checkout beta
+cd ..
 cp PiMAME/pimame_files/menu.py /home/pi/pimame_files/menu.py
 
 
