b7b07878565223bf06a888386484bf9e20fb9dc0
Shea <shea.silverman@ucf.edu>
2013-06-29 14:34:27 -0400

    Update update_include.sh

--- update_include.sh
+++ update_include.sh
@@ -29,11 +29,71 @@ ln -s /home/pi/emulators/mame4all-pi/roms/ /home/pi/roms/mame4all
 cd /home/pi/pimame_installer
 rm -rf mame4all-pi/
 
+###dgen
+wget http://sheasilverman.com/rpi/raspbian/installer/dgen.zip
+mv dgen.zip /home/pi/emulators/dgen.zip
+cd /home/pi/emulators/
+unzip -o dgen.zip
+mkdir /home/pi/roms/genesis
+rm dgen.zip
+cd /home/pi/pimame_installer
+chmod +x /home/pi/emulators/dgen-sdl-1.32/dgen
+
+
+###cavestory
+#wget http://sheasilverman.com/rpi/raspbian/installer/cavestory.zip
+wget https://github.com/vanfanel/cavestory_rpi/archive/master.zip
+mv master.zip /home/pi/emulators/cavestory.zip
+cd /home/pi/emulators/
+unzip -o cavestory.zip
+rm cavestory.zip
+cd /home/pi/pimame_installer
+cp /home/pi/pimame_installer/config_files/cs.sh /home/pi/emulators/cs.sh
+chmod +x /home/pi/emulators/cs.sh
+chmod +x /home/pi/emulators/cavestory_rpi-master/nx
+
+###dispmanx
+wget http://sheasilverman.com/rpi/raspbian/installer/SDL12-kms-dispmanx.zip
+unzip -o SDL12-kms-dispmanx.zip
+cd SDL12-kms-dispmanx
+sudo make install
+cd ..
+rm SDL12-kms-dispmanx.zip
+rm -rf SDL12-kms-dispmanx/
+
 #cd PiMAME
 #git checkout beta
 #cd ..
 cp PiMAME/pimame_files/menu.py /home/pi/pimame_files/menu.py
 
+##############pikeyd############
+wget http://sheasilverman.com/rpi/raspbian/installer/pikeyd.zip
+mv pikeyd.zip /home/pi/pimame_files
+cd /home/pi/pimame_files
+unzip -o pikeyd.zip
+mv pikeyd/pikeyd.conf ~/.pikeyd.conf
+if grep --quiet /home/pi/pimame_files/pikeyd /home/pi/.profile; then
+  echo "pikeyd already exists, ignoring."
+else
+  echo '/home/pi/pimame_files/pikeyd/pikeyd -d' >> /home/pi/.profile
+fi
+
+
+if sudo grep --quiet uinput /etc/modules; then
+  echo "Modules have already been added"
+else
+    sudo sh -c "echo 'uinput' >> /etc/modules"
+	sudo sh -c "echo 'i2c-dev' >> /etc/modules"
+fi
+
+if sudo grep --quiet '^blacklist i2c-bcm2708$' /etc/modprobe.d/raspi-blacklist.conf ; then
+    echo "Blacklisting i2c-bcm2708"
+    sudo sed -i '/blacklist i2c-bcm2708/d' /etc/modprobe.d/raspi-blacklist.conf
+    sudo sh -c "echo '#blacklist i2c-bcm2708' >> /etc/modprobe.d/raspi-blacklist.conf"
+else
+	echo "Module already blacklisted"
+fi
+###########
 
 #increment
-echo "PiMAME is now version 0.6 Beta 6.3"
+echo "PiMAME is now version 0.7.5"
